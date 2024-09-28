a894d76db14d9730fe93e10bd6cbb812fe124736
Sol M. Sánchez <somarielsanchez14@gmail.com>
2024-09-28 14:48:36 -0300

    prueba

--- js/clasificaciones.js
+++ js/clasificaciones.js
@@ -1,11 +1,11 @@
-// Leer las clasificaciones de localStorage
+// Leer las clasificaciones de localStorage y mostrar en la tabla
 window.onload = function() {
     const clasificaciones = JSON.parse(localStorage.getItem('clasificaciones')) || [];
     const tablaClasificaciones = document.querySelector('#tabla-clasificaciones tbody');
-  
+    
     // Limpiar la tabla
     tablaClasificaciones.innerHTML = '';
-  
+    
     // Llenar la tabla con las clasificaciones almacenadas
     clasificaciones.forEach(clasificacion => {
       const fila = document.createElement('tr');
@@ -13,7 +13,7 @@ window.onload = function() {
       const celdaPuntaje = document.createElement('td');
       const celdaFecha = document.createElement('td');
       const celdaJuego = document.createElement('td');
-  
+      
       celdaJugador.innerText = clasificacion.jugador;
       celdaPuntaje.innerText = clasificacion.puntaje;
       celdaFecha.innerText = new Date(clasificacion.fecha).toLocaleDateString(); // Convertir la fecha a formato legible
@@ -23,6 +23,7 @@ window.onload = function() {
       fila.appendChild(celdaPuntaje);
       fila.appendChild(celdaFecha);
       fila.appendChild(celdaJuego);
+      
       tablaClasificaciones.appendChild(fila);
     });
   };
